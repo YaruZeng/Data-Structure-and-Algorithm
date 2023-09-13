fedac8dc0697d92e4c8d0eb70d98d6fe797c5789
YARU ZENG <yaruzengivy@outlook.com>
2023-09-13 19:50:09 +0800

    Time: 2 ms (51.74%), Space: 9.5 MB (17.15%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        // use three pointers: two for traversing nums1 and nums2 in inverted order, one for store the larger number
+        
+        int i = m - 1; // index for nums1
+        int j = n - 1; // index for nums2
+        int k = m + n - 1; // index in nums1 to get correct number
+        
+        while (j >= 0) { // traverse all nums in nums2, if nums1 have more numbers, remain them as the correct order
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+};
