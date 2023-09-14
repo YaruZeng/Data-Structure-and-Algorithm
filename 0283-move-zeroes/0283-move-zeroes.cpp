2de1ad3dca7d3391fb4014b7b3383f8d2f7cfceb
YARU ZENG <yaruzengivy@outlook.com>
2023-09-14 10:04:14 +0800

    Time: 19 ms (56.66%), Space: 19.7 MB (7.50%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
+        // Solution1: 
         // define two pointers, left stores the non-zero numbers, right iterates numbers
-        int left = 0;
-        int right = 1;
+//         int left = 0;
+//         int right = 1;
         
-        while (right != nums.size()) {
-            if (nums[left] != 0) { // avoid situations where the first num is non-zero, e.g.[2,1]
-                left++;
-                right++;
-            } else if (nums[right] != 0) { // encounter a non-zero number
-                nums[left] = nums[right];
-                left++;
-                nums[right] = 0;
-                right++;
+//         while (right != nums.size()) {
+//             if (nums[left] != 0) { // avoid situations where the first num is non-zero, e.g.[2,1]
+//                 left++;
+//                 right++;
+//             } else if (nums[right] != 0) { // encounter a non-zero number
+//                 nums[left] = nums[right];
+//                 left++;
+//                 nums[right] = 0;
+//                 right++;
+//             } else {
+//                 right++;
+//             }
+//         }
+        
+        // Solution2: two pointers start from the same point, when encounter zero, j move forward till it encounters a non-zero num, exchange with i
+        int i = 0, j = 0;
+        while (i < nums.size() && j < nums.size()) {
+            if (nums[j] != 0) {
+                swap(nums[i++], nums[j++]);
             } else {
-                right++;
+                j++;
             }
         }
     }
