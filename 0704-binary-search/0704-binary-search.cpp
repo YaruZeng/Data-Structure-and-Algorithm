f02acd6224098a35140316fe37a9e383ff8dff72
YARU ZENG <yaruzengivy@outlook.com>
2023-09-14 20:15:40 +0800

    Time: 33 ms (32.14%), Space: 27.9 MB (10.64%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -16,3 +16,7 @@ public:
         return -1;
     }
 };
+
+// Time complexity: O(log n) - Since binary search reduces the search space by half at each step, the maximum number of iterations required to find the target is log base 2 of n, where n is the size of the array. Therefore, the time complexity of binary search is O(log n).
+
+// Space complexity: O(1)
