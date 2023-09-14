a47f398bf232929d2e73d7e6cbe665f58937deed
YARU ZENG <yaruzengivy@outlook.com>
2023-09-14 09:51:24 +0800

    Time: 11 ms (95.92%), Space: 19.7 MB (7.50%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -7,7 +7,7 @@ public:
         int right = 1;
         
         while (right != nums.size()) {
-            if (nums[left] != 0) {
+            if (nums[left] != 0) { // avoid situations where the first num is non-zero, e.g.[2,1]
                 left++;
                 right++;
             } else if (nums[right] != 0) { // encounter a non-zero number
