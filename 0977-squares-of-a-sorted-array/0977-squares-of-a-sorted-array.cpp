37865c73950801540f32fdb87105c787d133fc34
YARU ZENG <yaruzengivy@outlook.com>
2023-09-15 10:49:36 +0800

    Time: 23 ms (79.03%), Space: 26.1 MB (35.82%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -2,10 +2,25 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         // bruce force
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] = nums[i] * nums[i];
+        // for (int i = 0; i < nums.size(); i++) {
+        //     nums[i] = nums[i] * nums[i];
+        // }
+        // sort(nums.begin(), nums.end());
+        // return nums;
+        
+        // two pointers: start from two ends, get the larger one stored in the result
+        
+        vector<int> result(nums.size());
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        for (int k = nums.size() - 1; k >= 0; k--) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                result[k] = nums[left] * nums[left++];
+            } else {
+                result[k] = nums[right] * nums[right--];
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        return result;
     }
 };
