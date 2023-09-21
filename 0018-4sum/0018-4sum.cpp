e548ea0fcee94981c54033fc8697fe3611f491b1
YARU ZENG <yaruzengivy@outlook.com>
2023-09-21 12:34:59 +0800

    Time: 79 ms (40.18%), Space: 13.4 MB (46.34%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,18 +4,18 @@ public:
         vector<vector<int>> result;
         sort(nums.begin(), nums.end());
 
-        for (int k = 0; k < nums.size(); k++) {
+        for (int k = 0; k < nums.size(); k++) { // the first number
             if (nums[k] > target && nums[k] >= 0) break;
 
             if (k > 0 && nums[k] == nums[k - 1]) continue;
 
-            for (int i = k + 1; i < nums.size(); i++) {
-                if (nums[k] + nums[i] > target && nums[k] + nums[i] >= 0) break;
+            for (int i = k + 1; i < nums.size(); i++) { // the second number 
+                if (nums[k] + nums[i] > target && nums[k] + nums[i] >= 0) break; // cannot use return result, case: [-3,-2,-1,0,0,1,2,3] when [nums[k], nums[i]] is [-2,3]
 
                 if (i > k + 1 && nums[i] == nums[i - 1]) continue;
 
-                int left = i + 1;
-                int right = nums.size() - 1;
+                int left = i + 1; // the third number 
+                int right = nums.size() - 1; // the forth number 
 
                 while (left < right) {
                     if ((long) nums[i] + nums[k] + nums[left] + nums[right] < target) left++;
