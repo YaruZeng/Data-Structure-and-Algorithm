bf52349707ba91f6b93c3bf3798e24769d72cd5c
YARU ZENG <yaruzengivy@outlook.com>
2023-09-22 16:54:26 +0800

    Time: 0 ms (100.00%), Space: 8.8 MB (15.38%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,18 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Solution1: DFS, recursion
+
+// class Solution {
+// public:
+//     void traversal(vector<int>& vec, TreeNode* cur) {
+//         if (cur == NULL) return;
+//         traversal(vec, cur->left);
+//         traversal(vec, cur->right);
+//         vec.push_back(cur->val);
+//     }
+
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         traversal(result, root);
+//         return result;
+//     }
+// };
+
+// Solution2: BFS
+
 class Solution {
 public:
-    void traversal(vector<int>& vec, TreeNode* cur) {
-        if (cur == NULL) return;
-        traversal(vec, cur->left);
-        traversal(vec, cur->right);
-        vec.push_back(cur->val);
-    }
-
     vector<int> postorderTraversal(TreeNode* root) {
+        // postorder: left-right-mid -> mid-right-left
+        stack<TreeNode*> st;
         vector<int> result;
-        traversal(result, root);
+        if (root != NULL) st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            if (node != NULL) {
+                st.push(NULL);
+                if (node->right) st.push(node->right);
+                if (node->left) st.push(node->left);
+            } else {
+                st.pop();
+                node = st.top();
+                st.pop();
+                result.push_back(node->val);
+            }
+        }
         return result;
     }
 };
