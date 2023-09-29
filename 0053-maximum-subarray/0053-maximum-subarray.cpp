72b20a97c0544dbdf64cf1a60e0963ad2591d57d
YARU ZENG <yaruzengivy@outlook.com>
2023-09-29 10:12:34 +0800

    Time: 90 ms (22.95%), Space: 71 MB (5.35%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN;
-        int currentSum = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            currentSum += nums[i];
-            
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-            }
-            
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
+        if (nums.size() == 1) return nums[0];
+
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int result = nums[0]; // in case all nums < 0
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(nums[i], dp[i - 1] + nums[i]);
+            result = max(result, dp[i]);
         }
-        
-        return maxSum;
+        return result;
     }
 };
