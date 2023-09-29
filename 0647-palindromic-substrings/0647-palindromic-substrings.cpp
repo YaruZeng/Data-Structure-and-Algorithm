9e1b0dcdbd68cda908e1018d096f2d562a5754d8
YARU ZENG <yaruzengivy@outlook.com>
2023-09-29 12:00:39 +0800

    Time: 14 ms (41.37%), Space: 8.1 MB (53.32%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        int result = 0;
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            for (int j = i; j < s.size(); j++) {
+                if (s[i] == s[j]) {
+                    if (j - i <= 1) {
+                        dp[i][j] = true;
+                        result++;
+                    }
+                    else if (dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                        result++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
