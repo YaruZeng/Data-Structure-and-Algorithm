6ee9b4e35a647716cb3e3f5993a642bffaa33703
YARU ZENG <yaruzengivy@outlook.com>
2024-02-07 20:50:07 +0000

    Time: 91 ms (78.41%), Space: 73.1 MB (25.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         
-        // Solution1: time complexity O(n), space timplexity O(n)
-        // unordered_map<int, int> hash;
-        // for (int num : nums) {
-        //     hash[num]++;
-        //     if (hash[num] > 1) {
-        //         return true;
-        //     }
-        // }
-        // return false;
-        
-        // Solution2: time complexity O(nlogn), space timplexity O(1)
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i + 1] == nums[i]) return true;
+        unordered_set<int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hash.find(nums[i]) != hash.end()){
+                return true;
+            }
+            hash.insert(nums[i]);
         }
         return false;
     }
